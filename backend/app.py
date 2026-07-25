7117a123445226ff3fe2909d265626c66245e2d1
Soura <souravchauhan2442006@gmail.com>
2026-07-25 12:39:21 +0530

    Initial project setup

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {
+        "project": "VulnScan Lite",
+        "status": "Running",
+        "version": "1.0"
+    }
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5001, debug=True)
